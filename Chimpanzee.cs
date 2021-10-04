a8f27ebd44e2395aa72bbf1d3e54d37893e51156
NikitaTroyes <89306099+NikitaTroyes@users.noreply.github.com>
2021-10-04 22:12:37 +0200

    Create Chimpanzee.cs

---
+++ Chimpanzee.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Heritage
+{
+    class Chimpanzee : Animal 
+    {
+        public Chimpanzee(string name) : base(name, true) { }
+
+    }
+}
